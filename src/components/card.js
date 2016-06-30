4f4ecda55c497f7057fc470b7db64259ce12bc39
Francisco Juan <francisco.juan@klarna.com>
2016-06-30 14:55:12 +0200

    Cards are now flipped initially

--- src/components/card.js
+++ src/components/card.js
@@ -1,12 +1,31 @@
 import React from 'react'
 
 export default React.createClass({
+  getDefaultProps () {
+    return {
+      isFlipped: true
+    }
+  },
+
   render () {
     return (
-      <div className='card'
-           styles={this.props.cardGraphic.backgroundColor}
-           dangerouslySetInnerHTML={{__html: this.props.cardGraphic.svg}}>
+      <div className={this.divClassNames()}>
+        <a>
+          <div className='card-icon'
+               styles={this.props.cardGraphic.backgroundColor}
+               dangerouslySetInnerHTML={{__html: this.props.cardGraphic.svg}} />
+          <h2>{this.props.cardGraphic.label}</h2>
+        </a>
       </div>
     )
+  },
+
+  divClassNames () {
+    if (this.props.isFlipped) {
+      return 'card rotate'
+    } else {
+      return 'card'
+    }
+
   }
 })
