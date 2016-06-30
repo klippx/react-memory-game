dd8d6548e3f61ca8cb9a4602a44a7d7398b56e5a
Mathias Klippinge <mathias.klippinge@gmail.com>
2016-06-30 14:04:14 +0200

    WIP commit

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,33 @@
 import React from 'react'
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      gameRunning: false
+    }
+  },
+
   render () {
+    let body = (
+      <button onClick={this.startNewGame}>
+        New game
+      </button>
+    );
+
+    if (this.state.gameRunning) {
+      body = (<p>Placeholder for Game Component</p>);
+    }
+
     return (
       <div className='home-view'>
-        <h1>Hello React Memory Game</h1>
+        <h1>React Memory Game</h1>
+        {body}
       </div>
     )
+  },
+
+  startNewGame() {
+    this.setState({gameRunning: true})
+    console.log('Starting new game...')
   }
 })
